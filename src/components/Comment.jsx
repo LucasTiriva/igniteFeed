de0c47baaf7b03cdc6256bccdf513bd8485f12bc
LucasTiriva <lucastiriva@gmail.com>
2024-06-12 16:45:48 -0300

    like nos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
 
     return (
@@ -28,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>23</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
